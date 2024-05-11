0c72135247845528773baef02c1605f674688ce6
Jero Diacuna <jerodiacuna@gmail.com>
2024-05-11 22:38:38 +0800

    add shared preference
    if the user exits the appwithout logging out, it will continue its previous session but if the user logged out it will open the login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
+import 'package:firebase_auth/firebase_auth.dart';
 import 'login_screen.dart';
+import 'homepage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +21,48 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginScreen(), // Use LoginScreen as the home screen
+      home:
+          AuthenticationWrapper(), // Use AuthenticationWrapper as the home screen
     );
+  }
+}
+
+class AuthenticationWrapper extends StatefulWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+
+  @override
+  _AuthenticationWrapperState createState() => _AuthenticationWrapperState();
+}
+
+class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    // Check if the user is already signed in
+    _checkCurrentUser();
+  }
+
+  Future<void> _checkCurrentUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+    if (isLoggedIn) {
+      // User is logged in, navigate to home page
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    }
+  }
+
+  void _setLoggedIn(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedIn', value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginScreen(onLogin: _setLoggedIn);
   }
 }
